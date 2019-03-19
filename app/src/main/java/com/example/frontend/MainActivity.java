a5120d354bec507b7fe8f765dcc2175873328bf3
Nancy <butlern0513@gmail.com>
2019-03-19 11:54:56 -0700

    changed plain text search results to individual layouts

--- app/src/main/java/com/example/frontend/MainActivity.java
+++ app/src/main/java/com/example/frontend/MainActivity.java
@@ -6,9 +6,16 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -54,7 +61,8 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         mSearchText = findViewById(R.id.input_food_search);
-        usdaSearchRes = (TextView) findViewById(R.id.text_search_res);
+        //usdaSearchRes = (TextView) findViewById(R.id.text_search_res);
+
     }
 
 
@@ -66,15 +74,20 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(Void... params){
-            String url = "https://api.nal.usda.gov/ndb/search/?format=json&sort=n&max=25&offset=0&ds=Standard%20Reference&api_key=yWoagqfZMOoxfTOs9h3CI5siSLhvyc4MF791ZE5u" + "&q=" + mSearchText.getText().toString();
+            //combine search string with usda API
+            String url = "https://api.nal.usda.gov/ndb/search/?format=json&sort=r&max=25&offset=0&ds=Standard%20Reference&api_key=yWoagqfZMOoxfTOs9h3CI5siSLhvyc4MF791ZE5u" + "&q=" + mSearchText.getText().toString();
+           //initialize variables to null to run while loops
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
             String foodList = null;
 
             try {
+                //turn url string into URL type
                 URL urlHTTP = new URL(url);
+                //attempt to open connection with usda api
                 urlConnection = (HttpURLConnection) urlHTTP.openConnection();
                 InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                //StringBuffer is a modifiable string: use to store response
                 StringBuffer buffer = new StringBuffer();
 
                 if(in == null){
@@ -102,10 +115,47 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(String s) {
+        protected void onPostExecute(String s)  {
+            JSONObject foodObject;
+            JSONArray foodRes = null;
             super.onPostExecute(s);
-            usdaSearchRes.setText(s);
-            Log.i("json", s);
+            try {
+                foodObject = (JSONObject) new JSONTokener(s).nextValue();
+                foodRes = foodObject.getJSONObject("list").getJSONArray("item");
+            } catch (JSONException e){
+                Log.i("json", e.toString() );
+            }finally {
+                //usdaSearchRes.setText(foodRes.toString());
+
+
+                // Parent layout
+                LinearLayout parentLayout = (LinearLayout)findViewById(R.id.linear_food_res);
+
+                //remove previous search results
+                parentLayout.removeAllViews();
+
+                //use layoutinflater to add views (indiv food results) to scroll view
+                LayoutInflater layoutInflater = getLayoutInflater();
+                View view;
+
+                for(int i = 0; i < 25; i++){
+                   try{
+                        view = layoutInflater.inflate(R.layout.food_result, parentLayout, false );
+                        LinearLayout linearLayout = (LinearLayout)view.findViewById(R.id.food_res_layout);
+                        TextView foodName = (TextView)view.findViewById(R.id.text_food_name);
+                        TextView foodGroup = (TextView)view.findViewById(R.id.text_food_group);
+                        foodName.setText(foodRes.getJSONObject(i).getString("name"));
+                        foodGroup.setText(foodRes.getJSONObject(i).getString("group"));
+                        parentLayout.addView(view);
+                    } catch(JSONException e){
+                        Log.e("json", e.toString());
+                    }
+
+                }
+
+                Log.i("json", foodRes.toString());
+            }
+
         }
 
     }
